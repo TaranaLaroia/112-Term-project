44e340f90ab1c271c6398bb2c2aaa7575506a3ef
Taranasaurus <33974416+Taranasaurus@users.noreply.github.com>
2017-11-29 00:40:06 -0500

    Update sheetMusic.py

--- sheetMusic.py
+++ sheetMusic.py
@@ -1,47 +1,66 @@
 import wave
 import struct
 import sys
+from fft import *
 
-def wav_to_floats(wave_file):
-    w = wave.open(wave_file)
-    astr = w.readframes(w.getnframes())
-    # convert binary chunks to short 
-    a = struct.unpack("%ih" % (w.getnframes()* w.getnchannels()), astr)
-    a = [float(val) / pow(2, 15) for val in a]
-    ###PUT ABOVE LINE BACK
-    return a
+# def wav_to_floats(wave_file):
+#     w = wave.open(wave_file)
+#     astr = w.readframes(w.getnframes())
+#     # convert binary chunks to short 
+#     a = struct.unpack("%ih" % (w.getnframes()* w.getnchannels()), astr)
+#     a = [float(val) / pow(2, 15) for val in a]
+#     ###PUT ABOVE LINE BACK
+#     return a
 
-# read the wav file specified as first command line arg
-signal = wav_to_floats("/Users/taranalaroia/Documents/TERMPROJECT/testPyAudio.wav")
-#for i in range(0, (10)):
-    #print(signal[i])
+# # read the wav file specified as first command line arg
+signal = wav_to_floats("/Users/taranalaroia/Documents/TERMPROJECT/pianoG.wav")
+# #for i in range(0, (10)):
+#     #print(signal[i])
 
-print ("read "+str(len(signal))+" frames")
-print  ("in the range "+str(min(signal))+" to "+str(max(signal)))
+# print ("read "+str(len(signal))+" frames")
+# print  ("in the range "+str(min(signal))+" to "+str(max(signal)))
 
-import numpy as np
-j = np.complex(0,1)
-print("j is", j)
+# import numpy as np
+# j = np.complex(0,1)
+# print("j is", j)
+window=[]
+for i in range(256):
+    window.append(signal[0][i])
+#window=signal[0][:128]
+print(window)
+# for i in range(len(window)):
+#     window[i]*=(100.22727/4)
 
-window=signal[:16]
 
-import math
-def DFT(window): #window is a list of samples being transformed
-    samples=[]
-    magnitudes=[]
-    omega=2*math.pi/len(window)
-    for i in range(len(window)):
-        complexSample=(math.e**(-j*omega*i))*window[i]
-        samples.append(complexSample)
-        #print(samples[0])
-        #print(complexSample,np.absolute(complexSample),np.real(complexSample),np.imag(complexSample))
-        real=np.real(complexSample)
-        imag=np.imag(complexSample)
-        magnitudes.append(np.absolute(complexSample))
-        #agnitudes.append("potato")
-        #####GET MAGNITUDES OF SAMPLES
-    #print(samples)
-    return (magnitudes)
+sinWav=[]
+for i in range(256):
+    sinWav.append(7*math.sin((2*math.pi/8)*i))#+math.sin((2*math.pi/16)*i))
+
+frequencies=DFT2(window)
+#print(frequencies)
+# import math
+# def DFT(window): #window is a list of samples being transformed
+#     samples=[]
+#     magnitudes=[]
+#     omega=2*math.pi/len(window)
+#     max=(0,0)
+#     for i in range(len(window)):
+#         complexSample=(math.e**(-j*omega*i))*window[i]
+#         samples.append(complexSample)
+#         #print(samples[0])
+#         #print(complexSample,np.absolute(complexSample),np.real(complexSample),np.imag(complexSample))
+#         real=np.real(complexSample)
+#         imag=np.imag(complexSample)
+#         magnitude=np.absolute(complexSample)
+#         if magnitude>=max[0]:
+#             max=(magnitude,window[i],i*omega)
+#             #print("overwrite")
+#         # if abs(magnitude)>0.1:
+#         #     magnitudes.append((magnitude,i))
+#         #agnitudes.append("potato")
+#         #####GET MAGNITUDES OF SAMPLES
+#     #print(samples)
+#     return (max)
 
 import statistics
 def lowPassFilter(signal):
@@ -61,7 +80,7 @@ def downSample(signal):
     return signal[::4]
 
 
-print(DFT(signal))
+#print(DFT(window))
 
 
 # events-example0.py
@@ -91,15 +110,29 @@ def timerFired(data):
 
 def redrawAll(canvas, data):
     # draw in canvas
+    # canvas.create_rectangle(-10,-10,data.width+10,data.height+10,fill="#222222")
+    # dx=0
+    # for i in range(0, (500)):
+    #     color="lightskyblue"
+    #     if i%2==1:
+    #         color="indianred"
+    #     canvas.create_line(dx,data.height//2,dx,data.height//2+signal[i]*30000,width=1,fill=color)
+    #     dx+=1
     canvas.create_rectangle(-10,-10,data.width+10,data.height+10,fill="#222222")
     dx=0
-    for i in range(0, (500)):
+    for i in range(0, 256):
         color="lightskyblue"
         if i%2==1:
             color="indianred"
-        canvas.create_line(dx,data.height//2,dx,data.height//2+signal[i]*30000,width=1,fill=color)
-        dx+=1
+        if i==0:
+            color="red"
+        if i==256:
+            color="lightgreen"
+        canvas.create_line(dx,data.height,dx,data.height-frequencies[i][0]*.00030,width=2,fill=color)
+        dx+=2
 
+print(max(frequencies))
+print(framerate)
 
 ####################################
 # use the run function as-is
@@ -131,7 +164,7 @@ def run(width=300, height=300):
     data = Struct()
     data.width = width
     data.height = height
-    data.timerDelay = 100 # milliseconds
+    data.timerDelay = 10000 # milliseconds
     init(data)
     # create the root and the canvas
     root = Tk()
@@ -139,7 +172,7 @@ def run(width=300, height=300):
     canvas.pack()
     # set up events
     root.bind("<Button-1>", lambda event:
-                            mousePressedWrapper(evenbt, canvas, data))
+                            mousePressedWrapper(event, canvas, data))
     root.bind("<Key>", lambda event:
                             keyPressedWrapper(event, canvas, data))
     timerFiredWrapper(canvas, data)
@@ -147,4 +180,4 @@ def run(width=300, height=300):
     root.mainloop()  # blocks until window is closed
     print("bye!")
 
-#run(500, 800)
+run(520, 800)
